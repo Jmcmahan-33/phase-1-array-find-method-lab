9ba9e1e1f011a121f74806b87949a27795543d53
Jared McMahan <jaredm788@gmail.com>
2022-08-23 12:52:38 -0600

    commit tree 0379215aee02cc0ce822dfaee9b31f71248c102c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+// function superbowlWin(arrayRecord) {
+//     for(const year of arrayRecord) {
+//         if (year === "W") {
+//             return year
+//         }
+//     }
+// }
+
+
+
+// function superbowlWin(arrayRecord) {
+    
+//     let match = arrayRecord.find(obj => {
+//         return obj.result === "W"
+//     })
+//     if(match) {
+//         return match.year
+//     }
+//     console.log(match)
+// }
+
+
+function superbowlWin(arrayRecord) {
+        let match = arrayRecord.find(obj => {
+        return obj.result === "W"
+    })
+    if(match) {
+        return match.year
+    }
+
+}
